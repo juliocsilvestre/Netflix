84a278ba1343c22bd11112a5079cba537139b42b
juliocsilvestre <jcss.silvestre@gmail.com>
2022-12-14 17:08:44 -0300

    arquivos

---
+++ arquivos/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "46453ca8bd9091a176e41aaaa23bb477"
+
+let img_url = "https://image.tmdb.org/t/p/w500"
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+
+let movie_genres_http ="https://api.themoviedb.org/3/discover/movie?"
